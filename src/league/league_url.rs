902a6e525f0ea99efda499af7898e7a8e83a8e4e
dustytrash <Lionelbergen@live.com>
2020-10-27 18:17:33 -0500

    fixed snake case warnings

--- src/league/league_url.rs
+++ src/league/league_url.rs
@@ -10,11 +10,11 @@ impl LeagueUrl {
         return Self::get_url_from_api_key_with_summoner_id(Self::CHAMPION_MASTERY_BY_SUMMONER_ID, lol_api_key, summoner_id);
     }
 
-    fn get_url_from_api_key_with_summoner_id(original_url: &str, lol_api_key: &LolApiKey, summonerId: &str) -> String {
+    fn get_url_from_api_key_with_summoner_id(original_url: &str, lol_api_key: &LolApiKey, summoner_id: &str) -> String {
         original_url
             .replace("%region%", lol_api_key.region.get_code())
             .replace("%apikey%", &*lol_api_key.api_key)
-            .replace("%summonerid%", &summonerId)
+            .replace("%summonerid%", &summoner_id)
     }
 
     fn get_url_from_api_key(original_url: &str, lol_api_key: &LolApiKey) -> String {
